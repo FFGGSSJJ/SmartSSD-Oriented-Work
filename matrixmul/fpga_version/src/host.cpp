585f2949593a0de8b9961b2d057c2d2e1d7074bd
FF_GG <Guanshujie.19@intl.zju.edu.cn>
2022-05-31 14:14:32 -0500

    upload codes

---
+++ matrixmul/fpga_version/src/host.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file host.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-05-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
---
+++ matrixmul/fpga_version/src/matmul.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file matmul.cpp
+ * @author guanshujie fu
+ * @brief 
+ * @version 0.1
+ * @date 2022-05-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+// Includes
+#include <stdint.h>
+#include <hls_stream.h>
+
+/* define */
+#define PARALLEL    0
+
+
+#if PARALLEL
+static void loadrow(int32_t* in, hls::stream<int32_t>& inStream, int rowid, int row, int col) {
+    for (int i = 0; i < col; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = col max = col
+        inStream << in[rowid*row + i];
+    }
+}
+
+static void loadcol(int32_t* in, hls::stream<int32_t>& inStream, int colid, int row, int col) {
+    for (int i = 0; i < row; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = row max = col
+        inStream << in[i*row + colid];
+    }
+}
+#endif
+
+static void loadin(int32_t* in, hls::stream<int32_t>& inStream, int row, int col)
+{
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            inStream << in[i*row + j];
+        }
+    }
+}
+
+
+
+extern "C" {
+
+void matmul(int* matA, int* matB, int* outC){
+    return;
+}
+
+
+
+}
