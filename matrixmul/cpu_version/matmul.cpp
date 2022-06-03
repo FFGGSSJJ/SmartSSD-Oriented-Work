39bd3df3249ff78f35b55775951b11d52ac5fd22
FF_GG <Guanshujie.19@intl.zju.edu.cn>
2022-06-03 14:33:38 -0500

    update code

---
+++ matrixmul/cpu_version/matmul.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file matmul.cpp
+ * @author guanshujie fu
+ * @brief a cpp version of multiplication
+ * @version 0.1
+ * @date 2022-05-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
